c5d898139cb6c80610d7dc75f94c967a02aa52c8
Nguyensi15092005 <142871405+Nguyensi15092005@users.noreply.github.com>
2026-02-24 13:06:23 +0700

    update_1

--- dist/index.js
+++ dist/index.js
@@ -47,7 +47,7 @@ database.connect();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3006;
 var corsOptions = {
-    origin: 'https://benhvien-hospital.vercel.app',
+    origin: 'http://localhost:3007',
     credentials: true,
     optionsSuccessStatus: 200
 };
